0d2a7e7958758bcb189a5630f3fb4f5881a94255
Sergio Mir <voidtracer6@gmail.com>
2023-06-26 19:19:05 -0400

    firts commit

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -3,27 +3,29 @@ import React from "react"
 export const MainApp = () => {
 
   return (
-    <div className="flex flex-col justify-between">
-      <div className="divide-y-4 divide-slate-300 p-2 mx-4 mt-2 text-center flex flex-col">
-        <h1 className="bg-green-600 text-white p-4 rounded-xl text-lg">
-          Hello World!
-        </h1>
-        <h1 className="bg-yellow-600 text-white p-4 rounded-xl text-lg">
-          Hello World!
-        </h1>
+    <div className="">
+      <h1>This is a title</h1>
+      <h2>This is a subtitle</h2>
+      <p>This is a paragraph</p>
+      <a href="https://github.com/SergioMir018" className="">This is a anchor</a>
+      <div>
+        <button className="">This is a button</button>
       </div>
-      <div className="flex justify-center">
-          <a href="https://www.google.com" target="_blank" rel="noreferrer">
-            <button className="group border border-gray-200 p-1 rounded-lg font-bold text-sm text-gray-400  hover:border-blue-400 bg-white transition duration-300">
-              <span className="group-hover:text-red-500 transition duration-300">G</span>
-              <span className="group-hover:text-yellow-500 transition duration-300">o</span>
-              <span className="group-hover:text-green-500 transition duration-300">o</span>
-              <span className="group-hover:text-blue-500 transition duration-300">g</span>
-              <span className="group-hover:text-red-500 transition duration-300">l</span>
-              <span className="group-hover:text-yellow-500 transition duration-300">e</span>  
-          </button>
-          </a>
-        </div>
+      <div className="">
+        <input type="text" />
+      </div>
+      <div>
+        <input type="checkbox" id="checkbox" />
+        <label htmlFor="checkbox">Checkbox</label>
+      </div>
+      <div className="">
+        <select className="">
+          <option>Option 1</option>
+          <option>Option 2</option>
+        </select>
+      </div>
+      <h1>This is another title</h1>
+      <h2>This is another subtitle</h2>
     </div>
   )
 }
