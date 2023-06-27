28148cc64f596b9154efd111c1a100f9ca2b31eb
Sergio Mir <voidtracer6@gmail.com>
2023-06-27 15:09:48 -0400

    ./scr/MainApp.tsx: somo styles applied to the h1s, h2s & anchors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require("postcss-preset-env")({
+			stage: 1,
+			features: {
+				"nesting-rules": true,
+				"custom-properties": {
+					warnings: false,
+				},
+			},
+		}),
+	],
+};
 
